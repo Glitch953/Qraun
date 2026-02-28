603aebbf85bb3c1df9c3a7254a9953e7c0334e69
Ahmad Alhalwany <ahmedreer438i776666@outlook.com>
2026-02-28 21:49:03 +0300

    k

---
+++ .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_quran_[surahId]_page_actions_6119f74d.js
@@ -0,0 +1,3 @@
+module.exports=[72915,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_quran_%5BsurahId%5D_page_actions_6119f74d.js.map
---
+++ .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_quran_tasbih_page_actions_19050e88.js
@@ -0,0 +1,3 @@
+module.exports=[39542,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_quran_tasbih_page_actions_19050e88.js.map
